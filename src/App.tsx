e1e7f9bdc1baa7272eee8bb4f5c019138895da56
Mohamed Ibrahim AR <mohamedibrahim@presidio.com>
2024-10-21 17:37:00 +0530

    add basic counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 
+import "./App.css";
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount((c) => ++c);
+  }
+
+  function decrement() {
+    setCount((c) => --c);
+  }
+
   return (
     <BrowserRouter>
-      <div>
-        <h1
-          style={{
-            textAlign: "center",
-          }}
-        >
-          Hello World
-        </h1>
+      <div className="app">
+        <h1>Hello World</h1>
+        <div className="counter-container">
+          <button onClick={decrement}>-</button>
+          <h1>Count: {count}</h1>
+          <button onClick={increment}>+</button>
+        </div>
       </div>
     </BrowserRouter>
   );
