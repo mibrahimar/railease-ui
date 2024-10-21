6d1185b23c6e9a55c6c4ec9b9d61aede40b37652
Mohamed Ibrahim AR <mohamedibrahim@presidio.com>
2024-10-21 17:18:39 +0530

    start setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { appTheme } from "theme";
-import Pages from "components/pages";
-import { AuthProvider } from "store";
 
 function App() {
   return (
     <BrowserRouter>
-      <ChakraProvider theme={appTheme}>
-        <AuthProvider>
-          <Pages />
-        </AuthProvider>
-      </ChakraProvider>
+      <div>
+        <h1
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Hello World
+        </h1>
+      </div>
     </BrowserRouter>
   );
 }
